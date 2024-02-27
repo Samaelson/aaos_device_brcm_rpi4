ec927f13320c8b13fab878ef3aa37f32eda7670b
Samaelson <35295868+Samaelson@users.noreply.github.com>
2024-02-27 21:06:48 +0100

    added android.hardware.automotive.evs@1.1

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider-V1-external-service
+    android.hardware.camera.provider-V1-external-service \
+	android.hardware.automotive.evs@1.1
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/camera/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
